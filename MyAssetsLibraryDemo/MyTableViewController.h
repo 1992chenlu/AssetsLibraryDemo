de4ee247d9a317c39b944508d9a6eccf78569dd0
Chen Lu <2013luchen@gmail.com>
2015-07-08 15:49:58 -0400

    my AssetsLibrary demo commit NO1

---
+++ MyAssetsLibraryDemo/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  MyAssetsLibraryDemo
+//
+//  Created by 鲁辰 on 7/8/15.
+//  Copyright (c) 2015 ChenLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
--- MyAssetsLibraryDemo/MyTableViewController.m
+++ MyAssetsLibraryDemo/MyTableViewController.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  MyTableViewController.m
 //  MyAssetsLibraryDemo
 //
 //  Created by 鲁辰 on 7/8/15.
 //  Copyright (c) 2015 ChenLu. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "MyTableViewController.h"
 
-@interface ViewController : UIViewController
-
+@implementation MyTableViewController
 
 @end
-

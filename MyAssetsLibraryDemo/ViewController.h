8bee9f0c20e0879a8b4fdae3a4237c94e2289df8
Chen Lu <2013luchen@gmail.com>
2015-07-08 13:42:19 -0400

    Initial Commit

---
+++ MyAssetsLibraryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAssetsLibraryDemo
+//
+//  Created by 鲁辰 on 7/8/15.
+//  Copyright (c) 2015 ChenLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyAssetsLibraryDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyAssetsLibraryDemo
+//
+//  Created by 鲁辰 on 7/8/15.
+//  Copyright (c) 2015 ChenLu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
